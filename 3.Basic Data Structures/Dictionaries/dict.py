2ea068f58e623acc9120c90365560c3e15581da2
Krushna-Wankhade <kswankhade2110@gmail.com>
2025-02-10 00:33:41 +0530

    creata new  dictonary

---
+++ 3.Basic Data Structures/Dictionaries/dict.py
@@ -0,0 +1,14 @@
+my_dict = {
+    "name": "Krushna",
+    "age": 25,
+    "city": "Mumbai"
+}
+
+print(my_dict)
+
+
+#using dict() function 
+
+my_dict = dict(name="Krushna", age=25, city="Mumbai")
+
+print(my_dict)
