1c5e1cf8a8550b92231f625652b149ac6b525f0b
Krushna-Wankhade <kswankhade2110@gmail.com>
2025-02-10 00:38:31 +0530

    created new tuple

---
+++ 3.Basic Data Structures/Tuples/tup.py
@@ -0,0 +1,15 @@
+# Simple tuple
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+
+# Tuple with different data types
+mixed_tuple = (10, "Hello", 3.14, True)
+print(mixed_tuple)
+
+# Tuple with a single element (must include a comma)
+single_element_tuple = (42,)
+print(single_element_tuple)
+
+# Creating a tuple using the tuple() constructor
+tuple_from_list = tuple([1, 2, 3, 4])
+print(tuple_from_list)
