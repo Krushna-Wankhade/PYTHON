614023cf9186fca4c9285375d75127f78b2928f8
Krushna-Wankhade <kswankhade2110@gmail.com>
2025-02-10 00:35:30 +0530

    created new list

---
+++ 3.Basic Data Structures/Lists/list.py
@@ -0,0 +1,12 @@
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+
+# Printing the list
+print(my_list)
+
+
+my_list = []  # Empty list
+my_list.append(10)
+my_list.append(20)
+
+print(my_list)  # Output: [10, 20]
