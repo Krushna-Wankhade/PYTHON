e776a2f8987c75b2d484090e0b96b996adcae65d
Krushna-Wankhade <kswankhade2110@gmail.com>
2025-03-01 11:41:12 +0530

    Updated

---
+++ 4.Functions/function.py
@@ -0,0 +1,11 @@
+#Functiom is a group of statments that perform a specific task.
+#Function is used to reduce a redundancy and reduce the line of code.
+
+#Function defination
+def cal_sum(a,b): #parameters
+    sum=a+b
+    print(sum)
+    return sum
+
+cal_sum(2,3) #Function call; Argument
+cal_sum(3,3)
